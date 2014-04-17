b153834bd2220c9a6c686b569c4c80c350be2dc3
BLOOMS <bloomer.brian@gmail.com>
2014-04-17 16:15:41 -0700

    adding circleTest

---
+++ circleTest.py
@@ -0,0 +1,22 @@
+import unittest 
+#import circle
+
+class Circle:
+	'Understands a round plane figure whose boundary (circumference) consists of points equidistant from a fixed point (the center)'
+	def __init__(self, radius):
+		self.radius = radius
+	def area(self):
+		return 3.14*self.radius*self.radius
+
+class circleTests(unittest.TestCase):
+	def testShouldReturnAreaOfCircleWithRadius2(self):
+		actual = Circle(2).area()
+		expected = 12.56
+		self.assertEqual(actual, expected)
+
+
+def main():
+	unittest.main()
+
+if __name__ == "__main__":
+	unittest.main()
