0dbfaa54002c4978a074e435a0de72d2fc3a8d8b
BLOOMS <bloomer.brian@gmail.com>
2014-04-17 16:23:51 -0700

    added circumference and test

--- circleTest.py
+++ circleTest.py
@@ -7,10 +7,17 @@ class Circle:
 		self.radius = radius
 	def area(self):
 		return 3.14*self.radius*self.radius
+	def circumference(self):
+		return 3.14*self.radius*2
 
 class circleTests(unittest.TestCase):
 	def testShouldReturnAreaOfCircleWithRadius2(self):
 		actual = Circle(2).area()
+		expected = 12.56
+		self.assertEqual(actual, expected)
+
+	def testShouldReturnCircumferenceOfCircleWithRadius2(self):
+		actual = Circle(2).circumference()
 		expected = 12.56
 		self.assertEqual(actual, expected)
 
